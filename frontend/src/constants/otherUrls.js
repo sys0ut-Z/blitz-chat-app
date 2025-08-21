645c12eb8355efbc874163950745ccb28bb3f841
Daniel <zankhansql@gmail.com>
2025-08-21 22:04:33 +0530

    burl 2

--- frontend/src/constants/otherUrls.js
+++ frontend/src/constants/otherUrls.js
@@ -1,5 +1,5 @@
-// const BACKEND_URL = "http://localhost:5001";
-const BACKEND_URL = "https://blitz-chat-app-gnn2.onrender.com";
+const BACKEND_URL = "http://localhost:5001";
+// const BACKEND_URL = "https://blitz-chat-app-gnn2.onrender.com";
 
 export {
   BACKEND_URL
